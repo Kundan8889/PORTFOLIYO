c13257940a16331723be0e5e53d5474eccdce8a2
kundan8889 <kundanrathore450@gmail.com>
2024-06-07 02:44:02 +0530

    projects

--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import reportWebVitals from "./reportWebVitals";
 import { ThemeProvider } from './pages/Techstack/ThemeContext';
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <ThemeProvider>
@@ -12,3 +12,4 @@ root.render(
   </React.StrictMode>
   </ThemeProvider>
 );
+reportWebVitals();
